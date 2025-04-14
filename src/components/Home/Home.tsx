8d51a68811631af2ddd12011042f49faed419c6e
Kasador <hunterstevenshaw@gmail.com>
2025-04-13 22:04:37 -0700

    🎨🖌️ Fixed UI/UX, started adding about section.

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,7 @@
 import Me from "../../assets/images/me/me_cartoon.png"
 // import HeroImageDesktop from "../../assets/images/graphics/hero_image_desktop.png"
 // import HeroImageMobile from "../../assets/images/graphics/hero_image_mobile.png"
-import Test from "../../assets/images/graphics/forest/test_v10.png"
+import Test from "../../assets/images/graphics/forest/hero_image_desktop_test_v5.png"
 import HeaderText from "./HeaderText"
 
 type Home = {
@@ -11,13 +11,11 @@ type Home = {
 const Home = () => {
     return (
         <section id="Home">
-            <section>
                 <div id="HeroContent">
                     <img src={Me} alt="Hunter Steven Shaw, but in cartoon version" id="Me" />
                     <HeaderText />
                 </div>
                 <img src={Test} alt="Mountain & Pine Trees Graphic" id="BottomHero" />
-            </section>
         </section>
     )
 }
